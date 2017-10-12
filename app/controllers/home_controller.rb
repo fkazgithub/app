a405594fb4f8ba3ca4314dd3ca30f0c8dfb28989
府川和樹 <kazukifukawa1012@gmail.com>
2017-10-12 10:00:15 +0900

    create app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def top
+  end
+
+def about
+end
+
+def select
+	end
+
+
+end
---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,18 @@
+class NotesController < ApplicationController
+  def new
+  end
+
+def create
+	@note=Note.new
+
+	@note.title=params[:title]
+	@note.content=params[:content]
+@note.save
+end
+
+
+def index
+
+	@notes=Note.all
+	end
+end
